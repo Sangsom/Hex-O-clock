cd5a076e9b94717c1184019ac7b140164141e6ca
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-07-23 21:36:03 +0300

    Showing current time

--- Hex O'Clock/ViewController.swift
+++ Hex O'Clock/ViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
+extension NSDate {
+    func hour() -> Int {
+        let calendar = Calendar.current
+        return calendar.component(.hour, from: Date())
+    }
+
+    func minute() -> Int {
+        let calendar = Calendar.current
+        return calendar.component(.minute, from: Date())
+    }
+
+    func second() -> Int {
+        let calendar = Calendar.current
+        return calendar.component(.second, from: Date())
+    }
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet var hourLabel: UILabel!
+
+    let currentDate = NSDate()
+    var hour: Int!
+    var minute: Int?
+    var second: Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        showTime()
     }
 
+    func showTime() {
+        hour = currentDate.hour()
+        guard let hour = currentDate.hour() as Int? else { return }
+        guard let minute = currentDate.minute() as Int? else { return }
+        guard let second = currentDate.second() as Int? else { return }
 
+        hourLabel.text = "\(hour):\(minute):\(second)"
+    }
 }
 
